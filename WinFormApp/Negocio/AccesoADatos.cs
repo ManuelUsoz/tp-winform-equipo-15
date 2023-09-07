b768200801c090764f027ad7e17c038b8e99db25
IgnacioBeeBianco <105438704+IgnacioBeeBianco@users.noreply.github.com>
2023-09-07 19:48:25 -0300

    Clase AccesoADatos
    Se agrego la clase AccesoADatos y se hizo la función agregar y listar para Articulo (hay que conectar al server todavía)

---
+++ WinFormApp/Negocio/AccesoADatos.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class AccesoADatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get 
+            { 
+                return lector; 
+            }
+        }
+
+        public AccesoADatos()
+        {
+            conexion = new SqlConnection();//Hay que agregar el server cuando este disponible
+            comando = new SqlCommand();
+        }
+
+        public void consultar(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+            {
+                lector.Close();
+            }
+            conexion.Close();
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+    }
+}
--- WinFormApp/Negocio/ArticuloNegocio.cs
+++ WinFormApp/Negocio/ArticuloNegocio.cs
@@ -3,10 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Dominio;
 
 namespace Negocio
 {
-    internal class ArticuloNegocio
+    public class ArticuloNegocio
     {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoADatos datos = new AccesoADatos();
+
+            try
+            {
+                datos.consultar("");//hacer la consutla
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    //Llenar con los demas atributos de Articulo
+                    aux.CodNumero = (int)datos.Lector["CodNumero"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void agregar(Articulo nuevo)
+        {
+            AccesoADatos datos = new AccesoADatos();
+            try
+            {
+                datos.consultar("");//hacer la consulta
+                //Para Usar Luego
+                //datos.setearParametro("");
+                //datos.setearParametro("");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
