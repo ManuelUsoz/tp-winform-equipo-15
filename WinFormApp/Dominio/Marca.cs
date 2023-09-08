7891179fcb4c49fea01ddb71dfa5225fbb9b9da4
ManuelUsoz <miusoz5@icloud.com>
2023-09-08 20:03:07 -0300

    Se agrega clase Marca

---
+++ WinFormApp/Dominio/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Marca
+    {
+        public long Id { get; set; }
+        public string ImagenUrl { get; set; }
+        public long IdArticulo { get; set; }
+    }
+}
