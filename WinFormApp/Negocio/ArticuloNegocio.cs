a2f793a66a14bde457f85f3ab0a991b59cf491f8
IgnacioBeeBianco <105438704+IgnacioBeeBianco@users.noreply.github.com>
2023-09-10 11:52:10 -0300

    pequeño fix

--- WinFormApp/Negocio/ArticuloNegocio.cs
+++ WinFormApp/Negocio/ArticuloNegocio.cs
@@ -108,7 +108,7 @@ namespace Negocio
                 throw ex;
             }
         }
-        /*
+       
         public List<Articulo> GetArticulos(int idMarca, string field, string criteria, string value)
         {
             try
@@ -120,6 +120,6 @@ namespace Negocio
                 throw ex;
             }
         }
-        */
+        
     }
 }
