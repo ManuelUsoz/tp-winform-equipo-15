33af1051e9c380454cff8b9d2cd2ee3c3db0896a
ManuelUsoz <miusoz5@icloud.com>
2023-09-17 11:47:53 -0300

    Se commitea nuevos formularios de vistas de marcas y categorias

---
+++ WinFormApp/WinForm-App/MarcasViewForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_App
+{
+    public partial class MarcasViewForm : Form
+    {
+        public MarcasViewForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
